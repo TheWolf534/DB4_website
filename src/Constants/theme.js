a150e611d91b3357832a55890bad3df34fdbcdb4
Tom <tb2r@icloud.com>
2024-06-07 15:44:21 +0200

    theme- font,colors

---
+++ src/Constants/theme.js
@@ -0,0 +1,118 @@
+const COLORS = {
+    mainText_1: '#000000',
+    primary_1: '#7FB7BC',
+    primary_2: '#2E858D',
+    gradient_1: '#44CE1B',
+    gradient_2: '#BBDB44',
+    gradient_3: '#F7E379',
+    gradient_4: '#F2A134',
+    gradient_5: '#E51F1F',
+    grey: "#D9D9D9",
+    white: '#FFFFFF',
+    black: '#000000',
+};
+
+const FONT = {
+    title_1: {
+        fontFamily: "Montserrat",
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    bold_48: {
+        fontFamily: "Montserrat",
+        fontSize: 48,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    bold_30: {
+        fontFamily: "Montserrat",
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    bold_26: {
+        fontFamily: "Montserrat",
+        fontSize: 26,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    bold_20: {
+        fontFamily: "Montserrat",
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    bold_16: {
+        fontFamily: "Montserrat",
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+    },
+    base_20: {
+        fontFamily: "Montserrat",
+        fontSize: 16,
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+    },
+    base_16: {
+        fontFamily: "Montserrat",
+        fontSize: 16,
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+    },
+    error_16: {
+        fontFamily: "Montserrat",
+        fontSize: 16,
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+    },
+    smallInfo_12: {
+        fontFamily: "Montserrat",
+        fontSize: 12,
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+    },
+    italic_12: {
+        fontFamily: "Montserrat",
+        fontSize: 12,
+        fontWeight: 'normal',
+        fontStyle: 'italic',
+    },
+};
+
+const SIZES = {
+    // ---Example---
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
+
+const SHADOWS = {
+    // ---Example---
+    // small: {
+    //   shadowColor: "#000",
+    //   shadowOffset: {
+    //     width: 0,
+    //     height: 2,
+    //   },
+    //   shadowOpacity: 0.25,
+    //   shadowRadius: 3.84,
+    //   elevation: 2,
+    // },
+    // medium: {
+    //   shadowColor: "#000",
+    //   shadowOffset: {
+    //     width: 0,
+    //     height: 2,
+    //   },
+    //   shadowOpacity: 0.25,
+    //   shadowRadius: 5.84,
+    //   elevation: 5,
+    // },
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
